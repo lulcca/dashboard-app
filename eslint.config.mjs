315441b9a7d7a0fbdb242ceb99ab532a9e33141d
lulcca <lucca.jacomassi@hotmail.com>
2024-06-15 17:22:23 -0300

    :tada: start project

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt({
+  rules: {
+    'comma-dangle': ['warn', 'always-multiline'],
+    'eol-last': 'error',
+    'no-multi-spaces': ['error', { exceptions: { 'Property': false } }],
+    'no-trailing-spaces': 'error',
+    quotes: ['warn', 'single'],
+    semi: ['error', 'always'],
+    'sort-imports': 'error',
+    'sort-keys': 'error',
+    'sort-vars': 'error',
+  },
+});
