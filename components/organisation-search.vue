98a821f785d7c45b15cd4a04766527c184db6613
lulcca <lucca.jacomassi@hotmail.com>
2024-06-20 06:09:46 -0300

    :poop: change organisation-search temporarily

--- components/organisation-search.vue
+++ components/organisation-search.vue
@@ -1,43 +1,31 @@
 <template>
   <div class="max-w-2xl">
-    <form
-      class="flex items-center"
-      @submit.prevent="selectOrganisation"
-    >
-      <div class="relative w-full">
-        <div class="absolute flex inset-y-0 items-center pointer-events-none ps-3 start-0">
-          <Icon
-            color="#9ca3af"
-            name="octicon:organization-16"
-            size="20"
-          />
-        </div>
-
-        <input
-          id="simple-search"
-          v-model="searchTerm"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-10 p-2.5"
-          list="org-names"
-          placeholder="Search branch name..."
-          required
-          type="search"
-        >
-
-        <datalist id="org-names">
-          <option v-for="(name, index) in organisationNames" :key="index" :value="name" />
-        </datalist>
-      </div>
-
-      <button
-        class="p-2.5 ms-2 text-sm font-medium text-white bg-primary rounded-lg border hover:bg-accent"
-        type="submit"
-      >
+    <div class="relative w-full">
+      <div class="absolute flex inset-y-0 items-center pointer-events-none ps-3 start-0">
         <Icon
-          name="mdi:search"
+          color="#9ca3af"
+          name="octicon:organization-16"
           size="20"
         />
-      </button>
-    </form>
+      </div>
+
+      <select
+        id="simple-search"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-10 p-2.5"
+        data-cy="organisation-selector"
+        @change="selectOrganisation"
+      >
+        <option disabled selected>Select an organisation...</option>
+
+        <option
+          v-for="(name, index) in organisationNames"
+          :key="index"
+          :value="name"
+        >
+          {{ name }}
+        </option>
+      </select>
+    </div>
   </div>
 </template>
 
@@ -46,11 +34,8 @@ const emit = defineEmits<{ (e: 'nameSelected', searchTerm: string): void }>();
 
 const organisationNames = useStore().getOrganisationNames();
 
-const searchTerm = ref('');
-
-function selectOrganisation() {
-  if (searchTerm.value && !organisationNames.includes(searchTerm.value)) searchTerm.value = '';
-
-  emit('nameSelected', searchTerm.value);
+function selectOrganisation(event: Event) {
+  const target = event.target as HTMLInputElement;
+  emit('nameSelected', target?.value);
 };
 </script>
